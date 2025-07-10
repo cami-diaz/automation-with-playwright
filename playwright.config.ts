217e052e8e46dcd11223fef50125c06c0e213a9d
Camila Diaz <diazcamilae@gmail.com>
2025-07-10 14:08:29 -0300

    Initial setup

---
+++ playwright.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  timeout: 30000,
+  retries: 0,
+  fullyParallel: true,
+  reporter: "html",
+  use: {
+    headless: true,
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+  },
+
+  projects: [
+    {
+      name: "Chromium",
+      use: {
+        browserName: "chromium",
+        ...devices["Desktop Chrome"],
+      },
+    },
+    {
+      name: "Firefox",
+      use: {
+        browserName: "firefox",
+        ...devices["Desktop Firefox"],
+      },
+    },
+    {
+      name: "WebKit",
+      use: {
+        browserName: "webkit",
+        ...devices["Desktop Safari"],
+      },
+    },
+  ],
+});
