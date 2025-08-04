d591273d159ae3d9037c166ee3cc2b9d56ace59a
Camila Diaz <diazcamilae@gmail.com>
2025-08-04 14:21:21 -0300

    Se agregaron escenarios de backend

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -16,3 +16,55 @@ test("TC-7 Verificar inicio de sesión exitoso", async ({ page }) => {
   dashboardPage = new DashboardPage(page);
   await expect(dashboardPage.dashboardTitle).toBeVisible();
 });
+
+test("TC-11 Loguearse con nuevo usuario creado por backend", async ({
+  page,
+  request,
+}) => {
+  const email =
+    TestData.usuario[0].email.split("@")[0] +
+    Date.now().toString() +
+    "@" +
+    TestData.usuario[0].email.split("@")[1];
+  const response = await request.post("http://localhost:6007/api/auth/signup", {
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+      "Content-Type": "application/json",
+    },
+    data: {
+      firstName: TestData.usuario[0].nombre,
+      lastName: TestData.usuario[0].apellido,
+      email: email,
+      password: TestData.usuario[0].password,
+    },
+  });
+  expect(response.status()).toBe(201);
+
+  const responsePromiseLogin = page.waitForResponse(
+    "http://localhost:6007/api/auth/login"
+  );
+  await loginPage.fillAndSubmitForm({
+    email: email,
+    password: TestData.usuario[0].password,
+  });
+
+  const responseLogin = await responsePromiseLogin;
+  const responseBodyLoginJson = await responseLogin.json();
+
+  expect(responseLogin.status()).toBe(200);
+  expect(responseBodyLoginJson).toHaveProperty("token");
+  expect(typeof responseBodyLoginJson.token).toBe("string");
+  expect(responseBodyLoginJson).toHaveProperty("user");
+  expect(responseBodyLoginJson.user).toEqual(
+    expect.objectContaining({
+      id: expect.any(String),
+      firstName: TestData.usuario[0].nombre,
+      lastName: TestData.usuario[0].apellido,
+      email: email,
+    })
+  );
+
+  await expect(page.getByText("Inicio de sesión exitoso")).toBeVisible();
+  dashboardPage = new DashboardPage(page);
+  await expect(dashboardPage.dashboardTitle).toBeVisible();
+});
--- tests/registro.spec.ts
+++ tests/registro.spec.ts
@@ -71,3 +71,103 @@ test("TC-6 Verificar que el usuario np pueda registrarse con un correo ya existe
   await expect(page.getByText("Email already in use")).toBeVisible();
   await expect(page.getByText("Registro exitoso")).not.toBeVisible();
 });
+
+test("TC-8 Verificar registro exitoso con datos validos verificando respuesta de la API", async ({
+  page,
+}) => {
+  const email =
+    TestData.usuario[0].email.split("@")[0] +
+    "camilatest" +
+    Date.now().toString() +
+    "@" +
+    TestData.usuario[0].email.split("@")[1];
+
+  const usuarioNuevo = { ...TestData.usuario[0], email };
+  await registerPage.fillForm(usuarioNuevo);
+  // Verificar que la respuesta de la API sea exitosa - status code 201
+  const responsePromise = page.waitForResponse(
+    "http://localhost:6007/api/auth/signup"
+  );
+  await registerPage.hacerClickRegister();
+  const response = await responsePromise;
+  const responseBody = await response.json();
+
+  expect(response.status()).toBe(201);
+  expect(responseBody).toHaveProperty("token");
+  expect(typeof responseBody.token).toBe("string");
+  expect(responseBody).toHaveProperty("user");
+  expect(responseBody.user).toEqual(
+    expect.objectContaining({
+      id: expect.any(String),
+      email: usuarioNuevo.email,
+      firstName: usuarioNuevo.nombre,
+      lastName: usuarioNuevo.apellido,
+    })
+  );
+
+  await expect(page.getByText("Registro exitoso")).toBeVisible();
+});
+
+test("TC-9 Generar signup desde la API", async ({ page, request }) => {
+  const email =
+    TestData.usuario[0].email.split("@")[0] +
+    Date.now().toString() +
+    "@" +
+    TestData.usuario[0].email.split("@")[1];
+  const response = await request.post("http://localhost:6007/api/auth/signup", {
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+      "Content-Type": "application/json",
+    },
+    data: {
+      firstName: TestData.usuario[0].nombre,
+      lastName: TestData.usuario[0].apellido,
+      email: email,
+      password: TestData.usuario[0].password,
+    },
+  });
+  const responseBody = await response.json();
+  expect(response.status()).toBe(201);
+  expect(responseBody).toHaveProperty("token");
+  expect(typeof responseBody.token).toBe("string");
+  expect(responseBody).toHaveProperty("user");
+  expect(responseBody.user).toEqual(
+    expect.objectContaining({
+      id: expect.any(String),
+      firstName: TestData.usuario[0].nombre,
+      lastName: TestData.usuario[0].apellido,
+      email: email,
+    })
+  );
+});
+
+test("TC-10 Verificar comportamiento del front ante un error 500 en el registro", async ({
+  page,
+}) => {
+  const email =
+    TestData.usuario[0].email.split("@")[0] +
+    Date.now().toString() +
+    "@" +
+    TestData.usuario[0].email.split("@")[1];
+
+  // Interceptar la solicitud de registro y devolver un error 500
+  await page.route("**/api/auth/signup", (route) => {
+    route.fulfill({
+      status: 409,
+      contentType: "application/json",
+      body: JSON.stringify({ message: "Email already in use" }),
+    });
+  });
+
+  // Llenar el formulario. La navegación se hace en beforeEach.
+  await registerPage.firstNameInput.fill(TestData.usuario[0].nombre);
+  await registerPage.lastNameInput.fill(TestData.usuario[0].apellido);
+  await registerPage.emailInput.fill(email);
+  await registerPage.passwordInput.fill(TestData.usuario[0].password);
+
+  // Hacer clic en el botón de registro
+  await registerPage.registerButton.click();
+
+  // Verificar que se muestra un mensaje de error.
+  await expect(page.getByText("Email already in use")).toBeVisible();
+});
